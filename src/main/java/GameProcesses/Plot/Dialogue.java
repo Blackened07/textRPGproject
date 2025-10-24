a0bfb0ea5049f5c162a6a9036b8fd601df385411
Blackened <blackened.07@mail.ru>
2025-10-25 02:01:05 +1000

    DoneGameProcessLogic2

--- src/main/java/GameProcesses/Plot/Dialogue.java
+++ src/main/java/GameProcesses/Plot/Dialogue.java
@@ -26,11 +26,9 @@ public class Dialogue implements Printable, PrintableInterfaces {
         }
         this.keys.addAll(getKeys());
     }
-
     public String getKey(int index) {
         return keys.get(index);
     }
-
     public void addTextsToListsInMap(String[][] text) {
         for (int i = 0; i < text.length; i++) {
             for (int k = 0; k < text[i].length; k++) {
@@ -38,33 +36,25 @@ public class Dialogue implements Printable, PrintableInterfaces {
             }
         }
     }
-
-
     public List<String> getKeys() {
         return eventsTextsAndCommands.keySet()
                 .stream()
                 .sorted()
                 .toList();
     }
-
+    public int getInnerListSize(int index) {
+        return eventsTextsAndCommands.get(getKey(index)).size();
+    }
 
     /// PRINT BLOCK
     @Override
-    public void printEventWithoutCommands(int index) {
-        System.out.println(eventsTextsAndCommands.get(getKey(index)).getFirst());
-    }
-
+    public void printEventWithoutCommands(int index) {System.out.println(eventsTextsAndCommands.get(getKey(index)).getFirst());}
     @Override
     public void printEventTextAndCommands(int index) {
         for (int i = 0; i < getInnerListSize(index); i++) {
             System.out.println(eventsTextsAndCommands.get(getKey(index)).get(i));
         }
     }
-
-    public int getInnerListSize(int index) {
-        return eventsTextsAndCommands.get(getKey(index)).size();
-    }
-
     @Override
     public void features(Organism player) {
         PrintableInterfaces.super.features(player);
