0b98a3020dd7bbc28942a2168cbdb907e92eaf97
Blackened <blackened.07@mail.ru>
2025-10-27 22:51:23 +1000

    ModifiedGameScanner

--- src/main/java/main/java/GameProcesses/Services/GameScanner.java
+++ src/main/java/main/java/GameProcesses/Services/GameScanner.java
@@ -4,30 +4,24 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public interface GameScanner {
-    String ONE = "1";
-    String TWO = "2";
-    String THREE = "3";
-    String FOUR = "4";
-    Pattern PATTERN = Pattern.compile("\\D");
+
+    String NON_DIGITS_PATTERN = "\\D";
 
     default int gameScanner(Scanner sc, int innerList) {
-        String[] commandsNumbers = {ONE, TWO, THREE, FOUR};
-        int userCommand = 0;
+
+        int userCommand;
         while (true) {
             try {
-                String userText = sc.nextLine();
-                for (String commands : commandsNumbers) {
-                    if (userText.equals(commands)) {
-                        userCommand = Integer.parseInt(userText);
-                        break;
-                    } else throw new InvalidCommandException("Вы ввели: '" + userText + "'! Некорректные данные!");
-                }
-                if (userCommand < innerList) break;
-                else throw new InvalidCommandException("Вы ввели: '" + userText + "'! Некорректный номер команды!");
+                String userText = sc.nextLine().replaceAll(NON_DIGITS_PATTERN, "");
+                if (userText.isEmpty()) throw new InvalidCommandException("Вы ввели: '" + userText + "'! Введите числовое значение!");
+                userCommand = Integer.parseInt(userText.replaceAll(NON_DIGITS_PATTERN, ""));
+                if (userCommand > innerList)
+                    throw new InvalidCommandException("Вы ввели: '" + userCommand + "'! Некорректный номер команды!");
+                return userCommand;
             } catch (InvalidCommandException exceptions) {
                 System.out.println(exceptions.getMessage());
             }
         }
-        return userCommand;
+
     }
 }
